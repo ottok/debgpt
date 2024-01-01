478a443f44d8ddaa920af78b17c45e20bcabcd95
Mo Zhou <cdluminate@gmail.com>
2024-01-01 17:38:08 -0500

    make llm chat work

--- debgpt/llm.py
+++ debgpt/llm.py
@@ -3,11 +3,14 @@
 from typing import *
 import argparse
 import rich
+from rich.panel import Panel
 console = rich.get_console()
 import torch as th
 import numpy as np
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline, Conversation
 from prompt_toolkit import prompt
+import re
 
 
 class AbstractLLM(object):
@@ -24,6 +27,7 @@ class Mistral7B(AbstractLLM):
     https://docs.mistral.ai/models/
     https://huggingface.co/docs/transformers/model_doc/mistral
     https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
+    https://huggingface.co/docs/transformers/main/chat_templating
     '''
     model_id = 'mistralai/Mistral-7B-Instruct-v0.2'
 
@@ -44,29 +48,64 @@ class Mistral7B(AbstractLLM):
                        'pad_token_id': 2}
 
     @th.no_grad() 
-    def generate(self, text: Union[list,str]):
-        if isinstance(text, list):
-            encoded = self.tok.apply_chat_template(text, return_tensors='pt').to(0)
+    def generate(self, messages: Union[list,str]):
+        if isinstance(messages, list):
+            encoded = self.tok.apply_chat_template(messages, tokenize=True,
+                                                   return_tensors='pt',
+                                                   add_generatio_prompt=True).to(0)
             model_inputs = encoded.to(self.device)
             generated_ids = self.llm.generate(model_inputs, **self.kwargs)
-            decoded = self.tok.batch_decode(generated_ids)
-        elif isinstance(text, str):
-            encoded = self.tok([text], return_tensors='pt').to(0)
+        elif isinstance(messages, str):
+            encoded = self.tok([messages], return_tensors='pt').to(0)
             model_inputs = encoded.to(self.device)
             generated_ids = self.llm.generate(**model_inputs, **self.kwargs)
         else:
-            raise TypeError(f'text type {type(text)} is not supprted')
+            raise TypeError(f'messages type {type(messages)} is not supprted')
         decoded = self.tok.batch_decode(generated_ids)
         return decoded
 
-    def interactive(self):
-        while text := prompt('Prompt> '):
-            response = self.generate(text)
-            console.print('LLM> ', response[0])
+
+    def chat(self):
+        '''
+        https://huggingface.co/docs/transformers/main/en/main_classes/pipelines#transformers.ConversationalPipeline
+        '''
+        pipe = pipeline('conversational', model=self.llm, tokenizer=self.tok, device=self.device)
+        chat = Conversation()
+        try:
+            while text := prompt('Prompt> '):
+                chat.add_message({'role': 'user', 'content': text})
+                chat = pipe(chat)
+                console.print('LLM> ', chat[-1]['content'])
+        except EOFError:
+            pass
+        except KeyboardInterrupt:
+            pass
+
+
+    def interact(self):
+        '''
+        Interact while keeping the history.
+        '''
+        tokenized_chat = []
+        try:
+            while text := prompt('Prompt> '):
+                tokenized_chat.append({'role': 'user', 'content': text})
+                response = self.generate(tokenized_chat)[0]
+                console.print(Panel(response, title='DEBUG'), markup=False)
+
+                dedup = self.tok.apply_chat_template(tokenized_chat, tokenize=False)
+                response = response.lstrip(dedup)
+
+                response = re.sub(r'^.*\[/INST\]', '', response) # It should be greedy.
+                response = re.sub(r'</s>', '', response)
+                response = response.strip()
+                console.print('LLM> ', response, markup=False)
+        except EOFError:
+            pass
+        except KeyboardInterrupt:
+            pass
 
     def example(self):
-        decoded = self.generate('who are you?')
-        console.print(decoded)
         messages = [
                 {"role": "user", "content": "What is your favourite condiment?"},
                 {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
@@ -74,6 +113,7 @@ class Mistral7B(AbstractLLM):
                 ]
         decoded = self.generate(messages)
         console.print(decoded)
+        return decoded
 
 
 if __name__ == '__main__':
