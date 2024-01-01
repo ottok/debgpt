0867742eca20ab2bc927b851c570ee83cd2b30e0
Mo Zhou <cdluminate@gmail.com>
2024-01-01 16:14:41 -0500

    py: get rid of vllm and go back to transformers

--- debgpt/llm.py
+++ debgpt/llm.py
@@ -6,7 +6,7 @@ import rich
 console = rich.get_console()
 import torch as th
 import numpy as np
-import vllm
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from prompt_toolkit import prompt
 
 
@@ -21,34 +21,61 @@ class AbstractLLM(object):
 
 class Mistral7B(AbstractLLM):
     '''
+    https://docs.mistral.ai/models/
+    https://huggingface.co/docs/transformers/model_doc/mistral
     https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
-    https://github.com/vllm-project/vllm
     '''
-    __model_id__ = 'mistralai/Mistral-7B-Instruct-v0.2'
-    __impl__ = 'vllm'
+    model_id = 'mistralai/Mistral-7B-Instruct-v0.2'
 
-    def __init__(self, *, dtype: str='float16'):
+    def __init__(self):
         '''
         dtype: float32 requires 32GB CUDA memory.
                bfloat16 (default) requires CUDA compute > 8.0 due to vLLM itself.
                float16 has better compatibility than bfloat16 through vllm.
         '''
         super().__init__()
-        console.log(f'Mistral7B> Loading {self.__model_id__} ({dtype})')
-        self.llm = vllm.LLM(model='mistralai/Mistral-7B-Instruct-v0.2', dtype=dtype)
-        self.sampling_params = vllm.SamplingParams(temperature=0.8, top_p=0.95)
+        console.log(f'Mistral7B> Loading {self.model_id} (float16)')
+        self.llm = AutoModelForCausalLM.from_pretrained(self.model_id,
+                                                        torch_dtype=th.float16)
+        self.llm.to(self.device)
+        self.tok = AutoTokenizer.from_pretrained(self.model_id)
+        self.kwargs = {'max_new_tokens': 128, 'do_sample': True,
+                       'pad_token_id': 2}
 
-    def generate(self, prompt: Union[List,str]):
-        prompt = [prompt] if isinstance(prompt, str) else prompt
-        outputs = self.llm.generate(prompt, self.sampling_params)
-        return [(output.prompt, output.outputs[0].text) for output in outputs]
+    @th.no_grad() 
+    def generate(self, text: Union[list,str]):
+        if isinstance(text, list):
+            encoded = self.tok.apply_chat_template(text, return_tensors='pt').to(0)
+            model_inputs = encoded.to(self.device)
+            generated_ids = self.llm.generate(model_inputs, **self.kwargs)
+            decoded = self.tok.batch_decode(generated_ids)
+        elif isinstance(text, str):
+            encoded = self.tok([text], return_tensors='pt').to(0)
+            model_inputs = encoded.to(self.device)
+            generated_ids = self.llm.generate(**model_inputs, **self.kwargs)
+        else:
+            raise TypeError(f'text type {type(text)} is not supprted')
+        decoded = self.tok.batch_decode(generated_ids)
+        return decoded
 
     def interactive(self):
-        while message := prompt('Prompt>'):
-            response = self.generate(message)
-            console.print('LLM>', response[0][1])
+        while text := prompt('Prompt> '):
+            response = self.generate(text)
+            console.print('LLM> ', response[0])
+
+    def example(self):
+        decoded = self.generate('who are you?')
+        console.print(decoded)
+        messages = [
+                {"role": "user", "content": "What is your favourite condiment?"},
+                {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+                {"role": "user", "content": "Do you have mayonnaise recipes?"}
+                ]
+        decoded = self.generate(messages)
+        console.print(decoded)
+
 
 if __name__ == '__main__':
     llm = Mistral7B()
     import IPython
-    IPython.embed(color='neutral')
+    IPython.embed(colors'neutral')
