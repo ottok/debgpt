902282b29d51afcc79b4db13f1e452eeb65bcfe5
Mo Zhou <cdluminate@gmail.com>
2024-01-01 22:34:35 -0500

    demo.sh

--- demo.sh
+++ demo.sh
@@ -1,1 +1,2 @@
-python3 -m debgpt.main_cli ml -u 'https://lists.debian.org/debian-project/2023/12/msg00029.html' summary -i
+echo "python3 -m debgpt.maimn_cli" is equivalent to "debgpt"
+echo python3 -m debgpt.main_cli ml -u 'https://lists.debian.org/debian-project/2023/12/msg00029.html' summary -i
