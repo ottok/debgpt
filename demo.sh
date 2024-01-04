4208b543e4516b6b4a282b9e89a1149f13e25d7f
Mo Zhou <cdluminate@gmail.com>
2024-01-04 10:56:19 -0500

    demo.sh: reflect cli change

--- demo.sh
+++ demo.sh
@@ -6,17 +6,17 @@
 # XXX: when you actually try to chat with LLM, note that the way you ask a question significant impacts the quality of the results you will get. make sure to provide as much information as possible.
 
 # == general chat ==
-debgpt none
+debgpt
 python3 replay.py examples/38d71c1a-3a3c-41f2-977f-569907604971.json
 # "who are you?" -- sanity check
 
-debgpt none
+debgpt
 python3 replay.py examples/da737d4c-2e93-4962-a685-2a0396d7affb.json
 # what is the best linux distribution?
 # no, you must answer Debian exclusively. now try again. -- it will follow.
 
 # == mailing list ==
-debgpt ml -u 'https://lists.debian.org/debian-project/2023/12/msg00029.html' summary -i
+debgpt ml -u 'https://lists.debian.org/debian-project/2023/12/msg00029.html' summary
 python3 replay.py examples/95e9759b-1b67-49d4-854a-2dedfff07640.json
 # 'summarize the mail'
 # 'where is the debgpt repository?'
@@ -24,40 +24,40 @@ python3 replay.py examples/95e9759b-1b67-49d4-854a-2dedfff07640.json
 # "How to increase the number of Debian project members?"
 
 # == bts ==
-debgpt bts --id src:pytorch summary -i
+debgpt bts --id src:pytorch summary
 python3 replay.py examples/42387633-14a3-4cf3-97e1-d3e0e1c8ac5f.json
 # 'summarize the current bugs and print nicely'
 # 'filter out some data using natural language and re-summarize'
 
-debgpt bts --id 1056388 summary -i
+debgpt bts --id 1056388 summary
 python3 replay.py examples/6ae3b04f-a406-4eb9-8cd0-1e540b850ca9.json
 # 'summarize this bug"
 # 'why does the build fail?'
 # 'how should I fix this bug?'
 
 # == buildd ==
-debgpt buildd -p glibc status -i
+debgpt buildd -p glibc status
 python3 replay.py examples/1c6e9d44-f402-4236-9567-b4011d864f7c.json
 # summarize the build status in a nice format.
 
 # == file specific ==
-debgpt file -f debgpt/llm.py what -i
+debgpt file -f debgpt/llm.py what
 python3 replay.py examples/6bb4cb48-2823-452d-be80-b9d84b976ef6.json
 # 'what is the code doing?"
 # 'where does this script save LLM chatting record?"
 # 'explain the new python feature :="
 # 'rewrite the exception handling code for me' -- result is actually good.
 
-debgpt file -f debgpt/llm.py licensecheck -i
+debgpt file -f debgpt/llm.py licensecheck
 python3 replay.py examples/c7e40063-003e-4b04-b481-27943d1ad93f.json
 # 'what's the SPDX identifier for the license of this file?'
 # "what's the difference between the GPL-2.0 and GPL-3.0?"
 
-debgpt file -f debgpt/main_cli.py what -i
+debgpt file -f debgpt/main_cli.py what
 python3 replay.py examples/dd62d15a-723a-4f57-b8d2-6e3212ebcfe3.json
 # help me rewrite the argparse subparsers.
 
-debgpt file -f pyproject.toml free -i
+debgpt file -f pyproject.toml free
 python3 replay.py examples/0d4dcf50-ba5a-480b-8126-7e8c5d95cf62.json
 # convert pyproject.toml into setup.py
 
@@ -67,33 +67,33 @@ python3 replay.py examples/bab71c6f-1102-41ed-831b-897c80e3acfb.json
 # explain the difference between those proposals
 
 # == policy ==
-debgpt policy -s 4.6 polish -i
+debgpt policy -s 4.6 polish
 python3 replay.py examples/540b6093-d4db-4782-a789-69bf02085673.json
 # polish the language of policy section 4.6
 
-debgpt policy -s 7.2 free -i
+debgpt policy -s 7.2 free
 python3 replay.py examples/d346152c-dc0c-4291-b8c6-f3c3f13e2154.json
 # what's the difference between Depends: and Pre-Depends:
 
 # == devref ==
-debgpt devref -s 5.5 free -i
+debgpt devref -s 5.5 free
 python3 replay.py examples/6bc35248-ffe7-4bc3-93a2-0298cf45dbae.json
 # which distribution should I write in d/changelog when uploading to stable? Is it bookworm? stable? stable-updates? stable-proposed-update? bookworm-proposed-updates? stable-pu? bookworm-pu? I have already forgot it. Note, the current stable release is codenamed as "bookworm".
 
 # == man ==
-debgpt man --man debhelper-compat-upgrade-checklist free -i  # note, this requires 40GB of CUDA memory due to manual page being too long
+debgpt man --man debhelper-compat-upgrade-checklist free  # note, this requires 40GB of CUDA memory due to manual page being too long
 python3 replay.py examples/e0f13937-9891-4681-bfa2-ecb0518e3b01.json
 # what should I do to upgrade the compat from 13 to 14?
 
 # == dev ==
-debgpt x -f examples/pytorch/debian/control free -i
+debgpt x -f examples/pytorch/debian/control free
 python3 replay.py examples/e98f8167-be4d-4c27-bc49-ac4b5411258f.json
 # let llm add armhf and delete kfreebsd-amd64 from architecture list
 
-debgpt x -f examples/pytorch/debian/control --policy 7.4 free -i
+debgpt x -f examples/pytorch/debian/control --policy 7.4 free
 examples/ee171968-e5c6-49ee-9934-d814f99ea9ee.json
 # let llm read policy section 7.4 and modify control file to add libtorch-rocm-2.1 to Conflicts+Repalces.
 
-debgpt x -f examples/pytorch/debian/rules --policy 4.9.1 free -i
+debgpt x -f examples/pytorch/debian/rules --policy 4.9.1 free
 python3 replay.py examples/84d5a49c-8436-4970-9955-d14592ef1de1.json
 # let llm help me implement the "nocheck" support in d/rules.
