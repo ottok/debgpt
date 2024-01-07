54b308ac27009a3072780e75fbff91915c36532a
Mo Zhou <cdluminate@gmail.com>
2024-01-07 00:48:12 -0500

    license header

--- debgpt/debian.py
+++ debgpt/debian.py
@@ -1,8 +1,25 @@
-# Copyright (C) 2024 Mo Zhou <lumin@debian.org>
-# MIT/Expat License.
 '''
-This file is in charge of organizaing (debian specific) user prompts,
-which are subsequently sent through frontend to the backend for LLM to process.
+MIT License
+
+Copyright (c) 2024 Mo Zhou <lumin@debian.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 from typing import *
 import re
@@ -14,6 +31,11 @@ import subprocess
 import sys
 from .defaults import QUESTIONS
 
+__doc__ = '''
+This file is in charge of organizaing (debian specific) functions for loading
+texts from various sources, which are subsequently combined into the first
+prompt, and sent through frontend to the backend for LLM to process.
+'''
 
 ########################
 # Utility I/O functions
